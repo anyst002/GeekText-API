1ff7ddaf30874354a5bad387047538eb71cfd215
overtonjeffry <overtonjeffry@gmail.com>
2025-02-28 22:01:57 -0500

    Update DiscountPrice.java

--- src/main/java/geektext/DiscountPrice.java
+++ src/main/java/geektext/DiscountPrice.java
@@ -1,1 +1,9 @@
 
+// Discount books by publisher.
+//o Logic: Update the price of all books under a publisher by a discount
+//percent.
+//o HTTP Request Type: PUT or PATCH
+//o Parameters Sent: Discount percent, Publisher
+//o Response Data: None
+
+
