ab6d5860a935d54f70eac05afc692747ced63cf1
Andrew Nystrom <andrew.nystrom03@gmail.com>
2025-03-05 17:15:36 -0500

    Error handling, hateoas integration, feature completion

--- src/main/java/geektext/UserController.java
+++ src/main/java/geektext/UserController.java
@@ -1,9 +1,11 @@
 package geektext;
 
 import java.util.Date;
-import java.util.Optional;
+//TODO import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,65 +16,92 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserController {
-	//Setting up repositories
+	//Setting up repositories and assemblers
 	private @Autowired UserRepository userRepository;
 	private @Autowired CreditCardRepository ccRepository;
+	private @Autowired UserModelAssembler userAssembler;
 
 	//=====================
 	//== Testing methods ==
+	//==== TODO remove ====
 	//=====================
 	
-	//Returns all users as JSON
-	@ResponseBody
-	@GetMapping(path="/user/all")
-	public Iterable<User> getAllUsers() {
-		return userRepository.findAll();
-	}
-	
-	//Returns all credit cards as JSON
-	@ResponseBody
-	@GetMapping(path="/user/credit/all")
-	public Iterable<CreditCard> getAllCreditCards() {
-		return ccRepository.findAll();
-	}
-	
-	//Gets a user via id; returns the user info as JSON
-	@ResponseBody
-	@GetMapping(path="/user/id/{id}")
-	public Optional<User> getUserById(@PathVariable Integer id) {
-		return userRepository.findById(id);
-	}
-	
-	//Gets a credit card via id; returns the card info as JSON
-	@ResponseBody
-	@GetMapping(path="/user/credit/id/{id}")
-	public Optional<CreditCard> getCreditCard(@PathVariable Integer id) {
-		return ccRepository.findById(id);
-	}
+//	//Returns all users as JSON
+//	@ResponseBody
+//	@GetMapping(path="/user")
+//	public Iterable<User> getAllUsers() {
+//		return userRepository.findAll();
+//	}
+//	
+//	//Returns all credit cards as JSON
+//	@ResponseBody
+//	@GetMapping(path="/credit")
+//	public Iterable<CreditCard> getAllCreditCards() {
+//		return ccRepository.findAll();
+//	}
+//	
+//	//Gets a user via id; returns the user info as JSON
+//	@ResponseBody
+//	@GetMapping(path="/user/id/{id}")
+//	public Optional<User> getUserById(@PathVariable Integer id) {
+//		return userRepository.findById(id);
+//	}
+//	
+//	//Gets a credit card via id; returns the card info as JSON
+//	@ResponseBody
+//	@GetMapping(path="/credit/id/{id}")
+//	public Optional<CreditCard> getCreditCard(@PathVariable Integer id) {
+//		return ccRepository.findById(id);
+//	}
 	
 	//======================
 	//==== Main methods ====
 	//======================
 	
+	//Helper method for getting user id via username
+	private Integer findUserId(String username) {
+		//Get user id as Object for error checking
+		Object o = userRepository.findId(username);
+		
+		//Throw exception if user doesn't exist
+		if(o == null) {throw new UserNotFoundException(username);}
+		
+		//Otherwise id is valid int so cast back to Integer
+		return (Integer) o;
+	}
+	
 	//Gets a user via username; returns the user info as JSON
-	//TODO incorrect type/invalid input/missing/extra params
 	@ResponseBody
-	@GetMapping(path="/user/username/{username}")
-	public Optional<User> getUser(@PathVariable String username) {
-		int id = userRepository.findUserId(username);
-		return userRepository.findById(id);
+	@GetMapping(path="/user/{username}")
+	public ResponseEntity<?> getUser(@PathVariable String username) {
+		//Get the user
+		int id = findUserId(username);
+		User u = userRepository.findById(id).get();
+		
+		//Build a JSON model for the user
+		EntityModel<User> uModel = userAssembler.toModel(u);
+		
+		return ResponseEntity.ok(uModel);
 	}
 	
 	//Adds a user; returns nothing
-	//TODO sql injection, incorrect type/invalid input/missing/extra params, nonunique username
 	@ResponseBody
 	@PostMapping(path="/user/add")
-	public void addUser(
+	public ResponseEntity<?> addUser(
 	@RequestParam("username")                   String username, 
 	@RequestParam("password")                   String userPassword, 
 	@RequestParam(required=false, name="name")  String fullName, 
 	@RequestParam(required=false, name="email") String emailAddress, 
 	@RequestParam(required=false, name="home")  String homeAddress) {
+		//Throw exception if username is already taken
+		Object o = userRepository.findId(username);
+		if(o != null) {throw new DuplicateUsernameException(username);}
+		
+		//Throw exception if required params are empty
+		String e = "";
+		if(username.compareTo(e) == 0 
+		|| userPassword.compareTo(e) == 0) {throw new EmptyParamException();}
+		
 		//Create the user
 		User u = new User();
 		u.setUsername(username);
@@ -81,22 +110,28 @@ public class UserController {
 		u.setEmailAddress(emailAddress);
 		u.setHomeAddress(homeAddress);
 		
-		//Save the user to the database
+		//Save the user to the database	
 		userRepository.save(u);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 	//Adds a credit card for a user; returns nothing
-	//TODO sql injection, incorrect type/invalid input/missing/extra params
 	@ResponseBody
-	@PostMapping(path="/user/credit/add")
-	public void addCreditCard(
+	@PostMapping(path="/credit/add")
+	public ResponseEntity<?> addCreditCard(
 	@RequestParam("username")          String username, 
 	@RequestParam("cardNum")           Long cardNum, 
 	@RequestParam("cvv")               String cvv, 
 	@RequestParam("expDate") 
 	@DateTimeFormat(pattern="yyyy-MM") Date expDate, 
 	@RequestParam("cardholder")        String cardholder) {
-		int id = userRepository.findUserId(username);
+		int id = findUserId(username);
+		
+		//Throw exception if required params are empty
+		String e = "";
+		if(cvv.compareTo(e) == 0
+		|| cardholder.compareTo(e) == 0) {throw new EmptyParamException();}
 		
 		//Create the credit card
 		CreditCard c = new CreditCard();
@@ -108,30 +143,47 @@ public class UserController {
 		
 		//Save the card to the database
 		ccRepository.save(c);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 	//Updates a user's fields; returns nothing
-	//TODO sql injection, incorrect type/invalid input/missing/extra params
 	@ResponseBody
-	@PutMapping(path="/user/update/{username}")
-	public void updateUser(
+	@PutMapping(path="/user/{username}")
+	public ResponseEntity<?> updateUser(
 	@PathVariable String username, 
 	@RequestParam(required=false, name="password") String userPassword, 
 	@RequestParam(required=false, name="name")     String fullName, 
 	@RequestParam(required=false, name="email")    String emailAddress, 
 	@RequestParam(required=false, name="home")     String homeAddress) {
 		//Get the user
-		int id = userRepository.findUserId(username);
+		int id = findUserId(username);
 		User u = userRepository.findById(id).get();
 		
 		//Set the new user values
-		if(userPassword != null) {u.setUserPassword(userPassword);}
-		if(fullName != null)     {u.setFullName(fullName);}
-		if(emailAddress != null) {u.setEmailAddress(emailAddress);}
-		if(homeAddress != null)  {u.setHomeAddress(homeAddress);}
+		//Throw exception if params are empty (but not null)
+		String e = "";
+		if(userPassword != null) {
+			if(userPassword.compareTo(e) == 0) {throw new EmptyParamException();}
+			else {u.setUserPassword(userPassword);}
+		}
+		if(fullName != null)     {
+			if(fullName.compareTo(e) == 0) {throw new EmptyParamException();}
+			else {u.setFullName(fullName);}
+		}
+		if(emailAddress != null) {
+			if(emailAddress.compareTo(e) == 0) {throw new EmptyParamException();}
+			else {u.setEmailAddress(emailAddress);}
+		}
+		if(homeAddress != null)  {
+			if(homeAddress.compareTo(e) == 0) {throw new EmptyParamException();}
+			else {u.setHomeAddress(homeAddress);}
+		}
 		
 		//Save the user to the database
 		userRepository.save(u);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- src/main/java/geektext/UserRepository.java
+++ src/main/java/geektext/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 	//Finds user id via username
-	//TODO sql injection, nonexistent user
 	@Query(nativeQuery=true, value="SELECT id FROM user WHERE username = :input")
-	public Integer findUserId(@Param("input") String username);
+	public Object findId(@Param("input") String username);
 }
