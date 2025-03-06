52924117cae5ba2b6fdb9c69c98092aad81bf027
overtonjeffry <overtonjeffry@gmail.com>
2025-03-06 12:28:30 -0500

    Create book.java

---
+++ src/main/java/geektext/book.java
@@ -0,0 +1,48 @@
+//package com.example.bookdiscount.model;
+
+public class Book {
+    private int id;
+    private String title;
+    private String publisher;
+    private double price;
+
+    // Constructor, getters, and setters
+    public Book(int id, String title, String publisher, double price) {
+        this.id = id;
+        this.title = title;
+        this.publisher = publisher;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
