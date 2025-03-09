f6f525980111537a4cba356c4137578bcf3e9c19
davidddx <david0319072@gmail.com>
2025-03-08 22:21:14 -0500

    foreign key stuff for wishlist

--- src/main/java/geektext/User.java
+++ src/main/java/geektext/User.java
@@ -5,34 +5,31 @@ import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
-@Entity // This tells Hibernate to make a table out of this class
+@Entity
+@Table(name = "user", uniqueConstraints = 
+@UniqueConstraint(columnNames = {"id", "username"}))
 public class User {
-	@Id
-	@GeneratedValue
-	private Integer id;
-
-
-	// need this @OneToMany annotation because of foreign key & entity relationship stuff.
-	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-	@JsonIgnore // omits wishlists from json output.
-	private Set<Wishlist> wishlists = new HashSet<>();     
-	
-	private String username;
-	private String userPassword;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private @Nonnull @Id Integer id;
+	private @Nonnull String username;
+	private @Nonnull String userPassword;
 	private String fullName;
 	private String emailAddress;
 	private String homeAddress;
 
-	public Set<Wishlist> getWishlists() {
-		return wishlists;
-	}
+	//======================
+	//=== Getter methods ===
+	//======================
 	
 	public Integer getId() {
 		return id;
@@ -56,5 +53,36 @@ public class User {
 	
 	public String getHomeAddress() {
 		return homeAddress;
+	}
+	
+	//======================
+	//=== Setter methods ===
+	//======================
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+	public void setHomeAddress(String homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+	// need this @OneToMany annotation because of foreign key & entity relationship stuff.
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	@JsonIgnore // omits wishlists from json output.
+	private Set<Wishlist> wishlists = new HashSet<>();     
+	public Set<Wishlist> getWishlists() {
+		return wishlists;
 	}
 }
