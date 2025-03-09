aebd74a7934c40e223c0799d8d5f69056d65379d
overtonjeffry <overtonjeffry@gmail.com>
2025-03-09 13:31:56 -0400

    Update BookServicer.java
    cleaned up and added comments

--- src/main/java/geektext/BookServicer.java
+++ src/main/java/geektext/BookServicer.java
@@ -1,20 +1,30 @@
 package geektext;
 
+//import libraries
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
+// Indicates class is a service
 @Service
+
+//Book servicer class
 public class BookServicer {
 
     @Autowired
-    private BookRepository bookRepository;
+    private BookRepository bookRepository; // Create bookRepository
+    
+ // Retrieve books by genre
+    public List<Book> getBooksByGenre(String genre) {
+        return bookRepository.findByGenre(genre);
+    }
 
-    public void applyDiscount(double discountPercent, int publisherId) {
-        // Use the new method to find books by publisher_id
-        List<Book> books = bookRepository.findByPublisher_id(publisherId);
+    // AppyDiscount Method
+    public void applyDiscount(double discountPercent, int publisher_id) {
+        //method to find books by publisher_id
+        List<Book> books = bookRepository.findByPublisher_id(publisher_id);
 
-        // Loop through each book, apply the discount, and save
+        // Loop through each book, apply the discount, then save
         for (Book book : books) {
             double newPrice = book.getPrice() * (1 - discountPercent / 100);
             book.setPrice(newPrice);
