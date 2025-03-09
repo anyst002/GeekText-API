3e9cd091f007694b6e21fda42bea83283381127d
overtonjeffry <overtonjeffry@gmail.com>
2025-03-09 00:36:18 -0500

    Delete src/main/java/geektext/DiscountPrice.java

--- src/main/java/geektext/DiscountPrice.java
+++
@@ -1,9 +0,0 @@
-
-// Discount books by publisher.
-//o Logic: Update the price of all books under a publisher by a discount
-//percent.
-//o HTTP Request Type: PUT or PATCH
-//o Parameters Sent: Discount percent, Publisher
-//o Response Data: None
-
-
