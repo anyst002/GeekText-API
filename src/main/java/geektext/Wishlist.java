8f751d472ced2b1e7e89719cd9a216c75882e431
davidddx <david0319072@gmail.com>
2025-03-08 22:25:25 -0500

    stuff todo and a bunch of imports i forgot about

--- src/main/java/geektext/Wishlist.java
+++ src/main/java/geektext/Wishlist.java
@@ -1,9 +1,22 @@
 package geektext;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.Query;
+
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 public class Wishlist {
@@ -11,22 +24,25 @@ public class Wishlist {
 	@GeneratedValue
 	private Integer id;
 	
-	// this is how to deal with foreign keys.
+	// this is how to deal with foreign keys.    
 	private @ManyToOne
 	@JoinColumn(name = "user_id") 
-	User user;
+		User user;
 	
+	//private @OneToMany() BookEntity book;
 	private String wishlistName;
 	public Wishlist(String name, User u) {
 		user = u;
 		wishlistName = name;
-	}
-	// default constructor needed bc i got an error for not having it.
+	};
 	public Wishlist() {
-		
 	}
-
-	
+	/*
+	 * todo
+	@Transient
+	@ElementCollection
+	public Set<BookEntity> books = null; 
+*/
 	public Integer getUserId() {
 		return user.getId();
 	}
