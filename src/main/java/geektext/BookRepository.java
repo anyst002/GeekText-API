24837a1870423bcd89c11878480bfc4ed00469be
overtonjeffry <overtonjeffry@gmail.com>
2025-03-09 13:32:28 -0400

    Update BookRepository.java
    added comments

--- src/main/java/geektext/BookRepository.java
+++ src/main/java/geektext/BookRepository.java
@@ -1,12 +1,14 @@
 package geektext;
 
+//import libraries
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 
+// BookRepository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    // Use 'publisher_id' as the field name in the query method
+    // Use 'publisher_id' as the field name in the query method, might be able to remove later, added to help with some issues
 	@Query("SELECT b FROM Book b WHERE b.publisher_id = :publisher_id")
     List<Book> findByPublisher_id(int publisher_id);
+	List<Book> findByGenre(String genre);
 }
