f1dd6f1c86e47cdef82092d44a8fd262e7ab1ddf
abdiel1216 <abdipl1216@gmail.com>
2025-03-10 21:02:01 -0400

    Update ShoppingCart.java

--- src/main/java/geektext/ShoppingCart.java
+++ src/main/java/geektext/ShoppingCart.java
@@ -1,1 +1,50 @@
+package geektext;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "shopping_cart")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToMany
+    @JoinTable(
+        name = "cart_books",
+        joinColumns = @JoinColumn(name = "cart_id"),
+        inverseJoinColumns = @JoinColumn(name = "isbn")
+    )
+    private List<Book> books;
+
+    // Getters and setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
 
