12bc328bcf633e453250a9501b9f1df090bcca95
abdiel1216 <abdipl1216@gmail.com>
2025-03-13 11:40:13 -0400

    Update ShoppingCartRepository.java

--- src/main/java/geektext/ShoppingCartRepository.java
+++ src/main/java/geektext/ShoppingCartRepository.java
@@ -1,8 +1,7 @@
 package geektext;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
-    Optional<ShoppingCart> findByUserId(Integer userId);
-}  
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+    ShoppingCart findByUserId(Integer userId);
+}
