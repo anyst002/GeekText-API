429af1fe8a6fc648bd31ba2d8080efa75023e0df
davidddx <david0319072@gmail.com>
2025-03-15 15:34:48 -0400

    wishlist stuff for foreign key table lsits

--- src/main/java/geektext/Wishlist.java
+++ src/main/java/geektext/Wishlist.java
@@ -39,6 +39,9 @@ public class Wishlist implements Serializable{
 			Set<Lists> L;
 	@JsonIgnore
 	public Set<Lists> getLists() {
+		for(Lists x : L) {
+			System.out.println("LIST: " + x);
+		}
 		return L;
 	}
 	private String wishlistName;
@@ -51,8 +54,16 @@ public class Wishlist implements Serializable{
 	@Transient
 	@ElementCollection
 	public Set<Book> books;
+	/*
+	public void addBook(Book b) {
+		books.add(b);
+	}*/
 	public Integer getUserId() {
 		return user.getId();
+	}
+	@JsonIgnore
+	public User getUser() {
+		return user;
 	}
 	public Integer getId() {
 		return id;
@@ -60,8 +71,24 @@ public class Wishlist implements Serializable{
 	public String getWishlistName() {
 		return wishlistName;
 	}
+	@Override
 	public String toString() {
 		return "Wishlist{wishlist_id=" + id + ", wishlist_name=" + wishlistName + ", user_id=" + user.getId() + "}";
+	}
+	@Override
+	public boolean equals(Object o) {
+	       // If the object is compared with itself then return true  
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof Wishlist)) {
+            return false;
+        }
+        Wishlist w = (Wishlist)o;
+        if(w.getId() != this.getId()) {
+        	return false;
+        }
+        return true;
 	}
 
 }
