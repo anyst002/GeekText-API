de7f115faf55f2fbf521b6c09d08e1c77f3ceeca
davidddx <david0319072@gmail.com>
2025-03-22 12:49:30 -0400

    function to find books in cart

--- src/main/java/geektext/WishlistRepository.java
+++ src/main/java/geektext/WishlistRepository.java
@@ -3,22 +3,13 @@ package geektext;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-// i just remember changing something to jpa repository n it worked again
+// i just remember changing something to jpa repository & it worked again
 interface WishlistRepository extends JpaRepository<Wishlist, Integer> {
 
-	
-	/* todo
-	 * 	// this is the query i tested and it worked in mysql workbench
-	// SELECT * FROM geektext.book WHERE isbn IN(SELECT book_id FROM geektext.lists WHERE wishlist_id = [insert a wishlist id]);
-	@Query(nativeQuery=true, value="SELECT * FROM book WHERE isbn IN(SELECT book_id FROM lists WHERE wishlist_id = :wId)")
-
-	public Set<BookEntity> getBooksByWishlistId(@Param("wId") Integer wishlistId);
-	
-	@Query(nativeQuery = true, value = "SELECT wishlist_name FROM wishlist WHERE id = :wId")
-	public Set<Wishlist> getWishlistsById(@Param("wId") Integer wishlistId);
-	*/
-
+	@Query(nativeQuery=true, value="SELECT book_id FROM cart WHERE user_id = :input")
+	public Set<Long> findBookIdInCart(@Param("input") Integer uId);
 }
