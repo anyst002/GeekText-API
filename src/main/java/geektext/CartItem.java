68ed26f8844c109f3c62c160cddf0771175f2972
abdiel1216 <abdipl1216@gmail.com>
2025-03-22 21:04:17 -0400

    Update CartItem.java

--- src/main/java/geektext/CartItem.java
+++ src/main/java/geektext/CartItem.java
@@ -1,48 +1,39 @@
 package geektext;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "cart_item")
 public class CartItem {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne
-    private Book book;
 
     private int quantity;
 
     @ManyToOne
-    @JsonBackReference // This prevents circular reference
+    @JoinColumn(name = "book_isbn", referencedColumnName = "isbn", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "shopping_cart_id", referencedColumnName = "id", nullable = false)
     private ShoppingCart shoppingCart;
 
     public CartItem() {}
 
-    public CartItem(Book book, int quantity) {
+    public CartItem(ShoppingCart shoppingCart, Book book, int quantity) {
+        this.shoppingCart = shoppingCart;
         this.book = book;
         this.quantity = quantity;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public Book getBook() {
-        return book;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public ShoppingCart getShoppingCart() {
-        return shoppingCart;
-    }
-
-    public void setShoppingCart(ShoppingCart shoppingCart) {
-        this.shoppingCart = shoppingCart;
-    }
+    public Long getId() { return id; }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public Book getBook() { return book; }
+    public void setBook(Book book) { this.book = book; }
+    public ShoppingCart getShoppingCart() { return shoppingCart; }
+    public void setShoppingCart(ShoppingCart shoppingCart) { this.shoppingCart = shoppingCart; }
 }
+
