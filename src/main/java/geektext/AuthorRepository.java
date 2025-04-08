6a13168f0bc6606a20bb4048a87c501ff4fbf26e
lilums02 <166068955+lilums02@users.noreply.github.com>
2025-04-08 11:22:31 -0400

    Update AuthorRepository.java
    adding author feature

--- src/main/java/geektext/AuthorRepository.java
+++ src/main/java/geektext/AuthorRepository.java
@@ -1,5 +1,6 @@
 package geektext;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorRepository extends JpaRepository<Author, String> {
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
