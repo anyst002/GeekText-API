7370b93950df576192f1e60527c0e267302639df
overtonjeffry <overtonjeffry@gmail.com>
2025-04-10 22:11:12 -0400

    Update BookRepository.java
    Updated some comments

--- src/main/java/geektext/BookRepository.java
+++ src/main/java/geektext/BookRepository.java
@@ -8,8 +8,7 @@ import java.util.List;
 // BookRepository
 public interface BookRepository extends JpaRepository<Book, Long> {
 	
-	//Find by Publisher for discounting
-    // Use 'publisher_id' as the field name in the query method, might be able to remove later, added to help with some query issues
+	// Find by Publisher for discounting
 	@Query("SELECT b FROM Book b WHERE b.publisher_id = :publisher_id")
     List<Book> findByPublisher_id(int publisher_id);
 	
@@ -23,8 +22,8 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 	
 	
 	// Find top 10 books based on copies sold in descending order
-	//  query to get the top 10 books sorted by copies_sold in descending order limit 10
     @Query("SELECT b FROM Book b ORDER BY b.copies_sold DESC LIMIT 10")
     List<Book> findTop10Books();
+    
     }
 
