50c210fab0a2c74ff84868789060fab37a02e29f
overtonjeffry <overtonjeffry@gmail.com>
2025-04-10 22:07:18 -0400

    Update BookController.java
    Updated some comments

--- src/main/java/geektext/BookController.java
+++ src/main/java/geektext/BookController.java
@@ -18,6 +18,7 @@ public class BookController {
 
 //Patch mapping for discount based on publisher    
     @PatchMapping("/discount")
+    
     //Method to apply discount given requestParams
     public ResponseEntity<String> applyDiscount(
             @RequestParam("discountPercent") double discountPercent,
@@ -28,11 +29,12 @@ public class BookController {
             return ResponseEntity.badRequest().body("Invalid discount percentage. The value should be between 1 and 100.");
         }
 
-        System.out.println("Applying discount: " + discountPercent + "% to publisher_id: " + publisher_id);  // Can remove later, used for testing
+         System.out.println("Applying discount: " + discountPercent + "% to publisher_id: " + publisher_id);  
 
         try {
             // Call the service to apply the discount based on publisher
             bookServicer.applyDiscount(discountPercent, publisher_id);
+            
             return ResponseEntity.noContent().build();  // Return HTTP 204 (No Content) to show success
         } catch (Exception e) {
             return ResponseEntity.status(500).body("An error has occurred while applying the discount.");
@@ -44,7 +46,8 @@ public class BookController {
  
  //Method to return entity list of books given a requestParam genre
     public ResponseEntity<List<Book>> getBooksByGenre(@RequestParam("genre") String genre) {
-        // Log the genre that is being passed in the request, for testing, can remove for final
+        
+	 // Log the genre that is being passed in the request
         System.out.println("Received genre parameter: " + genre);
 
         List<Book> books = bookServicer.getBooksByGenre(genre);
@@ -52,12 +55,12 @@ public class BookController {
         // To check if no books are in genre
         if (books.isEmpty()) {
             // If no books are found, log result and return 204 No Content
-            System.out.println("No books found for genre: " + genre);  // Can remove later, used for testing
+            System.out.println("No books found for genre: " + genre);  
             return ResponseEntity.noContent().build();
         }
 
         // If books are found, log result and return the list
-        System.out.println("Books found: " + books); // Can remove later, used for testing
+         System.out.println("Books found: " + books); 
         return ResponseEntity.ok(books); // Return 200 OK & the list of books
     }
  
@@ -66,6 +69,7 @@ public class BookController {
  
  //Method to return entity list of books given a requestParam rating
     public ResponseEntity<List<Book>> getBooksByRating(@RequestParam("rating") double rating) {
+	 
         // Log the rating that is being passed in the request, for testing, can remove for final
         System.out.println("Received rating parameter: " + rating);
 
@@ -73,13 +77,14 @@ public class BookController {
         
         // To check if no books are >= rating
         if (books.isEmpty()) {
+        	
             // If no books are found, log result and return 204 No Content
-            System.out.println("No books found for rating: " + rating);  // Can remove later, used for testing
+              System.out.println("No books found for rating: " + rating);
             return ResponseEntity.noContent().build();
         }
 
         // If books are found, log result and return the list
-        System.out.println("Books found: " + books); // Can remove later, used for testing
+        System.out.println("Books found: " + books); 
         return ResponseEntity.ok(books); // Return 200 OK & the list of books
     }
  
@@ -88,8 +93,6 @@ public class BookController {
  public List<Book> getTopSellers() {
      return bookServicer.getTop10BestSellers();
  }
-
-
 
 
  }
